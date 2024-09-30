20da23d7794854deca8d7503058ed212cf1083b3
Kuroneko <ken.aster8@gmail.com>
2024-09-30 16:55:56 +0900

    モデルメソッドを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[github]
 
   validates :uid, uniqueness: { scope: :provider }, if: -> { uid.present? }
+
+  has_many :repositories
+
+  def self.form_omniauth(auth)
+    where(uid: auth.uid, provider: auth.provider).first_or_create! do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name
+      user.birth_day = Date.parse('1993-04-21')
+      user.tel_number = '12345678'.to_i
+    end
+  end
 end
