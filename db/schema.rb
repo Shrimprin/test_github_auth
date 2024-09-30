53e275601e1a3070a8ff4614625579b7fd33e2da
Kuroneko <ken.aster8@gmail.com>
2024-09-30 16:21:29 +0900

    DB関連を設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_30_042919) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_30_071833) do
   create_table "repositories", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "name"
@@ -27,8 +27,11 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_30_042919) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "uid"
+    t.string "provider", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
   add_foreign_key "repositories", "users"
