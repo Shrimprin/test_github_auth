554225e5e2412bd9a575cbc020ff4d31ccd0efff
Kuroneko <ken.aster8@gmail.com>
2024-10-13 21:58:04 +0900

    モデルにomniauthableを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[github]
 
   validates :uid, presence: true, uniqueness: { scope: :provider }
 end
