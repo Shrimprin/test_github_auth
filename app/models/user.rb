70fadc03d0a43d599c556ea86f6c710d5546070c
Kuroneko <ken.aster8@gmail.com>
2024-10-13 21:56:21 +0900

    モデルに制約を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :uid, presence: true, uniqueness: { scope: :provider }
 end
