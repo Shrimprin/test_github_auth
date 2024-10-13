b7e145c9c45a5f4eb38f8cc22108b71220dd78fc
Kuroneko <ken.aster8@gmail.com>
2024-10-13 22:13:10 +0900

    OAuthのコールバックを定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" } # devise/omniauth_callbacksの代わりに、users/omniauth_callbacksを使用する
   resources :repositories
   root to: "repositories#index"
 
