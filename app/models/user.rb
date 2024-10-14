8a17f8cad73873fa737e22c0fb9a98822cf405b2
Kuroneko <ken.aster8@gmail.com>
2024-10-15 07:59:32 +0900

    ログインをGitHubのみにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[github]
+  devise :omniauthable, omniauth_providers: %i[github]
 
   validates :uid, presence: true, uniqueness: { scope: :provider }
 
@@ -10,8 +9,7 @@ class User < ApplicationRecord
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0, 20]
+      user.email = auth.info.email # 消していいかも
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   # deviseのログアウト
   devise_scope :user do
-    get "/users/sign_out" => "devise/sessions#destroy"
+    get "sign_out", to: "devise/sessions#destroy", as: :destroy_user_session
   end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
